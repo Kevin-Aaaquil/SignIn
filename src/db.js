e307a2f386a9dd6afb17ef07b9d07cb657faf90f
Kevin-Aaaquil <eshansingh2409@gmail.com>
2021-06-06 00:13:55 +0530

    init commit

---
+++ src/db.js
@@ -0,0 +1,34 @@
+// initialize mongodb
+console.clear();
+
+require('dotenv').config();
+const chalk = require('chalk');
+
+const MongoClient = require('mongodb').MongoClient;
+let db;
+const connect = (async ()=>{
+    try{
+        const client = await MongoClient.connect(process.env.MONGO_URI,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            ignoreUndefined:true,
+        });
+        console.log(chalk.magenta("database connected"));
+        return client.db();
+    }
+    catch(err){
+        console.log(err);
+        process.exit(-1);
+    }
+})
+
+const DB = async ()=>{
+    if(!db){
+        db=await connect();
+    }
+    else {
+    }
+    return db;
+}
+
+module.exports = DB;
